1cc00590e5fe1a9361395572e4378c810adb6e48
pankaj123-oss <57318079+pankaj123-oss@users.noreply.github.com>
2022-12-14 17:55:42 +0530

    1 file modified

--- app/main-page.js
+++ app/main-page.js
@@ -1,5 +1,11 @@
 import { createModel } from './main-view-model';
+import { Observable } from '@nativescript/core';
 export function LoadedTo(args) {
   const page = args.object;
-  page.bindingContext = createModel();
+  page.bindingContext = onTap();
+}
+export function onTap() {
+  const vm = new Observable();
+  var name = vm.get('name');
+  console.log(name);
 }
