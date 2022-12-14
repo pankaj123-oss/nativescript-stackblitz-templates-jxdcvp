1664664baf2475e7a63a98a779b524b6905c9d59
pankaj123-oss <57318079+pankaj123-oss@users.noreply.github.com>
2022-12-14 18:34:29 +0530

    2 files modified

--- app/main-page.js
+++ app/main-page.js
@@ -1,7 +1,7 @@
 import { createModel } from './main-view-model';
 import { Observable } from '@nativescript/core';
 var abc = createModel();
-var usernametest = "pankaj";
+var usernametest = 'pankaj';
 
 export function loadedTo(args) {
   const page = args.object;
@@ -9,6 +9,7 @@ export function loadedTo(args) {
 }
 export function onTap(args) {
   var name = abc.get('name');
-
+  var pass = abc.get('pass');
   console.log(name);
+  console.log(pass);
 }
--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,9 +1,5 @@
 import { Observable } from '@nativescript/core';
 export function createModel() {
   const vm = new Observable();
-  // vm.onTap = () => {
-  //   var name = vm.get('name');
-  //   console.log(name);
-  // };
   return vm;
 }
